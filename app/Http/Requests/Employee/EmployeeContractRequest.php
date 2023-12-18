8be4dce58c1f6bd10459f18daa6bb96b21200df9
sunil <sunilgangadhar.infanion.com>
2023-12-18 15:46:46 +0530

    fixes

--- app/Http/Requests/Employee/EmployeeContractRequest.php
+++ app/Http/Requests/Employee/EmployeeContractRequest.php
@@ -14,14 +14,14 @@ class EmployeeContractRequest extends ApiRequest
     public function rules(): array
     {
         $rules = [
-            'employee_profile_id'       => new EmployeeLinkedToCompanyRule(getCompanyId()),
+            'employee_profile_id'       => [new EmployeeLinkedToCompanyRule(getCompanyId())],
             'employee_contract_details' => ['bail', 'required', 'array', new EmployeeContractDetailsRule()],
             'employee_function_details' => ['bail', 'required', 'array', new EmployeeFunctionDetailsRule()],
         ];
 
-        if ($this->isMethod('PUT') || $this->isMethod('PATCH')) {
-            unset($rules['employee_profile_id']);
-        }
+        // if ($this->isMethod('PUT') || $this->isMethod('PATCH')) {
+        //     unset($rules['employee_profile_id']);
+        // }
 
         return $rules;
     }
