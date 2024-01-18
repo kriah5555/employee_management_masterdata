08c05bdd566bdf022c8cf874e383b7e9011ae3cf
root <root@ip-172-31-4-104.eu-west-1.compute.internal>
2024-01-18 06:13:28 +0000

    fixes

--- app/Services/Employee/EmployeeContractService.php
+++ app/Services/Employee/EmployeeContractService.php
@@ -123,13 +123,11 @@ class EmployeeContractService
             $employeeType = EmployeeType::findOrFail($contractDetails['employee_type_id']);
             $employeeContract = $this->employeeContractRepository->getEmployeeContractById($employee_contract_id);
             $employee_profile_id = $employeeContract->employee_profile_id;
-
             $this->employeeContractRepository->updateEmployeeContract($employee_contract_id, [
                 'start_date'       => $contractDetails['start_date'],
                 'end_date'         => $contractDetails['end_date'],
                 'employee_type_id' => $contractDetails['employee_type_id'],
             ]); # update contract details
-
             if ($employeeType->employeeTypeCategory->id == config('constants.LONG_TERM_CONTRACT_ID')) { # update long data if contract cat id long term
                 $employeeContract->load('longTermEmployeeContract'); // Load the relationship
 
--- app/Services/Planning/PlanningService.php
+++ app/Services/Planning/PlanningService.php
@@ -274,7 +274,9 @@ class PlanningService implements PlanningInterface
     {
         $plannings = $this->getDayPlannings($location, $workstations, $employee_types, $date);
         $absenceService = app(AbsenceService::class);
-	    return $plannings->map(function ($plan) use ($absenceService) {
+	return $plannings->map(function ($plan) use ($absenceService) {
+		$leave_status = $absenceService->getAbsenceForDate($plan->plan_date)->isNotEmpty();
+
             return [
                 'plan_id'                  => $plan->id,
                 'plan_date'                => $plan->plan_date,
